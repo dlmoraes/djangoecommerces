08ba5b7b3aec865e81560160a66ff3f7f3dce32f
dlmoraes <diegofrost96@gmail.com>
2016-08-09 01:10:17 -0300

    adicionando secret key via variável de ambiente

---
+++ djangoecommerce/urls.py
@@ -0,0 +1,13 @@
+from copy import name
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from core import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^contato/$', views.contact, name="contact"),
+    url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
+    url(r'^admin/', admin.site.urls),
+]
