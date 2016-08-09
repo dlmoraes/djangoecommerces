d92dd4387b4d00f31900f42f001f1837d02af2a6
dlmoraes <diegofrost96@gmail.com>
2016-08-09 01:27:47 -0300

    alterando catalog admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,12 +8,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created', 'modified']
     search_fields = ['name', 'slug']
     list_filter = ['created', 'modified']
+    prepopulated_fields = {'slug': ('name',)}
 
 class ProductAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'category', 'created', 'modified']
     search_fields = ['name', 'slug', 'category__name']
     list_filter = ['created', 'modified']
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
